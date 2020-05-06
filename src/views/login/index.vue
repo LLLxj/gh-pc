a3883d6ed8afc0b8e57049862a68c544e42c43b8
LLLxj <253358001@qq.com>
2020-05-06 17:03:18 +0800

    内增登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <el-button @click="login()">
+      登录
+    </el-button>
+  </div>
+</template>
+
+<script>
+import system from '@/api/system'
+export default {
+   name: 'login',
+    components: {
+    },
+    data() {
+      return {
+        dataform: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      // nextTo () {
+      //   console.log(11)
+      //   this.$router.push({path:'login'}) 
+      // }
+      login() {
+        system.login(this.dataform).then(res => {
+          console.log(res)
+        }).catch(err => {
+          console.log(err)
+        })
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
