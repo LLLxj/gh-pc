eaa54a61af0d8bc415610371f616f09d8e74d201
LLLxj <253358001@qq.com>
2020-05-06 17:00:06 +0800

    更新入口文件main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from '@/router' // 路由
+import './plugins/element.js'
+
 
 Vue.config.productionTip = false
 
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
