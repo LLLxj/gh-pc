e476d3ed4aab0f11bfe7da1c5e9f18a758c4504d
LLLxj <253358001@qq.com>
2020-05-06 17:03:25 +0800

    新增注册页面

---
+++ src/views/register/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div @click="nextTo">register</div>
+</template>
+
+<script>
+  export default {
+    name: 'register',
+    components: {
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      nextTo () {
+        this.$router.push({path:'login'}) 
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
