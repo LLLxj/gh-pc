4440ed385f0972d8f4f38fb0a921edb2506a5e2b
LLLxj <253358001@qq.com>
2020-05-06 17:01:55 +0800

    新增layout模块

---
+++ src/views/layout/components/header/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="page">
+    <header class="menu">
+      <ul>
+        <li v-for="(item,index) in routers" :key="index" @click="pageTo(item)">{{item.title || "你好," + name}}</li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        name: '测试',
+        routers: [
+          { title: '' },
+          { title: '首页', path:'/home' },
+          { title: '登录', path:'/login' },
+          { title: '注册', path:'/register' },
+          { title: '权益中心', path:'/proCenter' },
+          { title: '个人中心', path:'/perCenter' },
+        ],
+        route: ''
+      }
+    },
+    watch: {  
+      $route (val) {
+        this.route = val
+      }
+    },
+    methods: {
+      pageTo (item) {
+        if (item.path) {
+          this.$router.push({path: item.path})
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scope>
+  .page {
+    width:100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    background: rgba(0, 0, 0, 0.8);
+    .menu {
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+        li {
+          list-style: none;
+          padding:15px 20px;
+          color:#fff;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="app-wrap">
+    <div class="header">
+      <Header></Header>
+    </div>
+
+    <div class="content">
+      <router-view />
+    </div>
+    
+    <div class="footer">
+      <Footer></Footer>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script>
+  import Header from './components/header'
+  import Footer from './components/footer'
+  export default {
+    name: 'home',
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      Header, Footer
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style lang="scss" scope>
+  .app-wrap {
+    width: 100%;
+    margin:0;
+    padding:0;
+    .left{
+      width:700px;
+    }
+    .right {
+    }
+    .margin {
+      margin-top:10px;
+    }
+    .header {
+      width:100%;
+    }
+    .content {
+      width:62.5%;
+      margin:0 auto;
+      min-width: 1200px;
+    }
+    .footer {
+      width:100%;
+       border:1px solid blue;
+      height:100px;
+    }
+  }
+</style>
