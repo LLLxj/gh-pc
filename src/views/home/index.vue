c0202895676e03cca6782550951d22fbc1e96709
LLLxj <253358001@qq.com>
2020-05-06 17:03:11 +0800

    新增首页页面

---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pages">
+
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scope> 
+  .pages {
+    border:1px solid red;
+  }
+</style>
